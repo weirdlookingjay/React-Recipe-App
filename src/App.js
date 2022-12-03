517c6434210fc5a31dbc318f85a202c7d5b9641c
Joe Clark <weirdlookingjay@live.com>
2022-12-03 18:55:23 -0500

    Created Navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">Boilerplate</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div className="navbar container">
+      <a href="#!" className="logo">
+        <span>Recipes</span>Hub
+      </a>
+      <div className="nav-links">
+        <a href="#!">Home</a>
+        <a href="#!">Recipes</a>
+        <a href="#!">Settings</a>
+      </div>
+    </div>
+  );
+}
