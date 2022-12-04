8b345496cb20e1cce2ec9cb50c634437f1fbf38a
Joe Clark <weirdlookingjay@live.com>
2022-12-04 07:14:08 -0500

    Added hamburger menu to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 export default function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false);
   return (
     <div className="navbar container">
       <a href="#!" className="logo">
@@ -8,6 +11,13 @@ export default function Navbar() {
         <a href="#!">Home</a>
         <a href="#!">Recipes</a>
         <a href="#!">Settings</a>
+      </div>
+      <div
+        className={showSidebar ? "sidebar-btn active" : "sidebar-btn"}
+        onClick={() => setShowSidebar(!showSidebar)}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
       </div>
     </div>
   );
