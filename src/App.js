91b50f437041d5d33d8a5263c1145b074fa31bed
Joe Clark <weirdlookingjay@live.com>
2022-12-04 14:00:06 -0500

    Added Hero section and Images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <div className="container main">
+        <HeroSection />
+      </div>
     </div>
   );
 }
