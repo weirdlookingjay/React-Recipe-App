522f6fc611936530cb1d18aafc4481dba9d3b3ef
Joseph Clark <weirdlookingjay@live.com>
2022-12-05 13:32:50 -0500

    Added Improve Skills Section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import HeroSection from "./components/HeroSection";
+import ImproveSkills from "./components/ImproveSkills";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Navbar />
       <div className="container main">
         <HeroSection />
+        <ImproveSkills />
       </div>
     </div>
   );
